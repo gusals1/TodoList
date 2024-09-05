1964b4740ee129acd718832306405391da90167b
qkrgus3318@gmail.com <qkrgus3318@gmail.com>
2024-09-06 08:10:20 +0900

    myTodoList

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,38 @@
-/** @type {import('next').NextConfig} */
+import withTM from "next-transpile-modules";
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.js$/,
+        include: /node_modules\/(antd|@ant-design\/icons)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["next/babel"],
+            plugins: [
+              [
+                "import",
+                {
+                  libraryName: "antd",
+                  libraryDirectory: "es",
+                  style: true,
+                },
+              ],
+            ],
+          },
+        },
+      });
+    }
+    return config;
+  },
 };
 
-export default nextConfig;
+export default withTM([
+  "antd",
+  "rc-util",
+  "rc-pagination",
+  "@ant-design/icons",
+  "rc-picker",
+])(nextConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+// import "antd/dist/antd.css";
+import { globalStyles } from "@/src/common/styles/globalStyles";
+import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
